d0849a837c154f1b8c17c1f946da54f3eef733d3
hertter <hertter@qq.com>
2019-03-27 17:51:31 +0800

    代码雨

---
+++ CodeRain/coderain/Constant.java
@@ -0,0 +1,15 @@
+package cn.hertter.coderain;
+
+public class Constant {
+    final static int FONT_SIZE = 20;
+    final static int LABEL_SIZE = 25;
+    final static int TEXT_PANE_HEIGHT = 25;
+    /**
+     * 每个TextPane之间的间距
+     */
+    final static int PADDING = 30;
+    /**
+     * 每个TextPane初始的Y坐标
+     */
+    final static int DEFAULT_Y = 30;
+}
